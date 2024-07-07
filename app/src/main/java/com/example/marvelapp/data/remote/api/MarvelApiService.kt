e893cce251ff22ff79fd13fc5c4d5e0718bdde49
Brady <brady@openharbor.finance>
2024-07-08 00:00:15 +0900

    add setup-retrofit 내용 : retrofit setup

---
+++ app/src/main/java/com/example/marvelapp/data/remote/api/MarvelApiService.kt
@@ -0,0 +1,17 @@
+package com.example.marvelapp.data.remote.api
+
+import com.example.marvelapp.data.remote.dto.MarvelApiResponseDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MarvelApiService {
+    @GET("characters")
+    suspend fun getCharacters(
+        @Query("nameStartsWith") name: String,
+        @Query("limit") limit: Int = 10,
+        @Query("offset") offset: Int = 0,
+        @Query("apikey") apiKey: String,
+        @Query("ts") timestamp: String,
+        @Query("hash") hash: String
+    ): MarvelApiResponseDto
+}
