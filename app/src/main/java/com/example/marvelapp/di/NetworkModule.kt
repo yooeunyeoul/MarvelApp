b21b22879eaa6eaa89c0400a305f47a744e29737
Brady <brady@openharbor.finance>
2024-07-08 02:48:47 +0900

    add api 추가 내용 : di 추가

--- app/src/main/java/com/example/marvelapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/marvelapp/di/NetworkModule.kt
@@ -1,13 +1,17 @@
 package com.example.marvelapp.di
 
+import com.example.marvelapp.BuildConfig
 import com.example.marvelapp.data.remote.api.MarvelApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -18,11 +22,17 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
+    fun provideHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
         return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
             .build()
     }
-
     @Provides
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
@@ -38,4 +48,15 @@ object NetworkModule {
     fun provideMarvelApiService(retrofit: Retrofit): MarvelApiService {
         return retrofit.create(MarvelApiService::class.java)
     }
+
+    @Provides
+    @Singleton
+    @Named("apiKey")
+    fun provideApiKey(): String = BuildConfig.MARVEL_PUBLIC_KEY
+
+    @Provides
+    @Singleton
+    @Named("privateKey")
+    fun providePrivateKey(): String = BuildConfig.MARVEL_PRIVATE_KEY
+
 }
