41a3d825227dceb6f020abddf4785f555f525e2d
Brady <brady@openharbor.finance>
2024-07-07 20:51:49 +0900

    add initial setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.hiltPlugin)
+    alias(libs.plugins.kapt)
 }
 
 android {
@@ -51,19 +53,51 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    dependencies {
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.lifecycle.runtime.ktx)
+        implementation(libs.androidx.activity.compose)
+        implementation(platform(libs.androidx.compose.bom))
+        implementation(libs.androidx.ui)
+        implementation(libs.androidx.ui.graphics)
+        implementation(libs.androidx.ui.tooling.preview)
+        implementation(libs.androidx.material3)
+
+        // lifecycle compose
+        implementation(libs.lifecycle.runtime.compose)
+
+        // navigation compose
+        implementation(libs.navigation.compose)
+
+        // Hilt
+        implementation(libs.hilt.android)
+        kapt(libs.hilt.compiler)
+        implementation(libs.hilt.navigation.compose)
+
+        // retrofit
+        implementation(libs.retrofit)
+        implementation(libs.retrofit.gson)
+        implementation(libs.gson)
+
+        // coil
+        implementation(libs.coil.compose)
+
+        // swipe
+        implementation(libs.androidx.compose.material)
+//    implementation(libs.androidx.compose.material.swipe.to.refresh)
+
+        // Test
+        testImplementation(libs.junit)
+        testImplementation(libs.mockito.core)
+        testImplementation(libs.mockito.inline)
+        testImplementation(libs.coroutines.test)
+        testImplementation(libs.kotlin.test.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+        androidTestImplementation(platform(libs.androidx.compose.bom))
+        androidTestImplementation(libs.androidx.ui.test.junit4)
+        debugImplementation(libs.androidx.ui.tooling)
+        debugImplementation(libs.androidx.ui.test.manifest)
+    }
+
 }
