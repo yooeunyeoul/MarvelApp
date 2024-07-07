218374a0884ead39cf939e94eddefb90e51d6bde
Brady <brady@openharbor.finance>
2024-07-08 02:47:38 +0900

    add repository module 추가 내용 : provideCharacterRepository 추가

---
+++ app/src/main/java/com/example/marvelapp/di/RepositoryModule.kt
@@ -0,0 +1,35 @@
+package com.example.marvelapp.di
+
+import com.example.marvelapp.data.local.dao.FavoriteDao
+import com.example.marvelapp.data.remote.api.MarvelApiService
+import com.example.marvelapp.data.repositoryimpl.CharacterRepositoryImpl
+import com.example.marvelapp.data.repositoryimpl.FavoriteRepositoryImpl
+import com.example.marvelapp.domain.repository.CharacterRepository
+import com.example.marvelapp.domain.repository.FavoriteRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideCharacterRepository(
+        apiService: MarvelApiService,
+        @Named("apiKey") apiKey: String,
+        @Named("timestamp") timestamp: String,
+    ): CharacterRepository {
+        return CharacterRepositoryImpl(apiService, apiKey, timestamp)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFavoriteRepository(favoriteDao: FavoriteDao): FavoriteRepository {
+        return FavoriteRepositoryImpl(favoriteDao)
+    }
+}
