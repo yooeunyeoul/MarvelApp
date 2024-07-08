5bd25deefaacc76ae42070804fa850d507add133
Brady <brady@openharbor.finance>
2024-07-08 13:35:56 +0900

    edit repository 수정으로 인한 usecase 수정

--- app/src/main/java/com/example/marvelapp/domain/usecase/GetFavoriteCharactersUseCase.kt
+++ app/src/main/java/com/example/marvelapp/domain/usecase/GetFavoriteCharactersUseCase.kt
@@ -3,12 +3,16 @@ package com.example.marvelapp.domain.usecase
 import com.example.marvelapp.domain.mapper.toUiModel
 import com.example.marvelapp.domain.repository.FavoriteRepository
 import com.example.marvelapp.presentation.model.MarvelUiCharacter
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetFavoriteCharactersUseCase @Inject constructor(
     private val favoriteRepository: FavoriteRepository
 ) {
-    suspend operator fun invoke(): List<MarvelUiCharacter> {
-        return favoriteRepository.getFavoriteCharacters().map { it.toUiModel() }
+    suspend operator fun invoke(): Flow<List<MarvelUiCharacter>> {
+        return favoriteRepository.getFavoriteCharacters().map { characters ->
+            characters.map { it.toUiModel() }
+        }
     }
 }
